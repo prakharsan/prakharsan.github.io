747bad56bc610a783ea8b049caf67bc2a78f7b9f
Prakhar Sankrityayan <39276008+prakharsan@users.noreply.github.com>
2024-05-18 15:44:11 +0530

    fix: updating linkedin handle

--- assets/js/app.js
+++ assets/js/app.js
@@ -202,7 +202,7 @@ let footer = $(`
     </svg>
     </a>
 
-    <a class="social-button linkedin" href="https://www.linkedin.com/in/prakharsankrityayan/" target="_blank" >
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/prakharsan/" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
